80eeed6923cbc71261576e42bb6b8c13475ba2e3
Tiago Ribeiro Traça <tiago.traca@codengage.com>
2022-03-21 08:03:20 -0300

    feat(back-end):iadd student model

---
+++ api/src/models/students.model.js
@@ -0,0 +1,29 @@
+const Sequelize = require("sequelize");
+const db = require("../db/index");
+
+const Student = db.define("student", {
+  id: {
+    primaryKey: true,
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  cpf: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  academic_register: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Student;
